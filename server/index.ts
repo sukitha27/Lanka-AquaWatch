ea80cd9177a04c7cac951439248c778548ed32d2
suki <142525137+sukitha27@users.noreply.github.com>
2025-12-06 13:25:00 +0000

    new

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import connectPgSimple from "connect-pg-simple";
-import { registerRoutes } from "./routes.js";
+import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import { Pool } from "pg";
